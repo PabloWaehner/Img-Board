07ebe5dfc70ce77470f9afa603d8cb925f32301e
Pablo Waehner <waehnermp@gmail.com>
2018-09-23 20:32:10 +0200

    heroku

--- sql/comments.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS comments;
-
-CREATE TABLE comments (
- id SERIAL PRIMARY KEY,
- image_id INTEGER REFERENCES images(id),
- username VARCHAR(100) NOT NULL,
- comment TEXT NOT NULL,
- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
- );
